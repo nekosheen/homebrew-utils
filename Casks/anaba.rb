879bd8565d003cd2c13b9c942740793b4d328420
Nacio-Felix <naciofelix.laubressac@ipsosante.fr>
2020-06-24 16:06:27 +0200

    Update: Style

--- Casks/anaba.rb
+++ Casks/anaba.rb
@@ -1,10 +1,11 @@
-cask 'anaba' do
-  version '1.3.0'
-  sha256 '958c6fa435a72e44bcce47ff8824fbfa81cd10c29a8b6f090b99de435303e45f'
+cask "anaba" do
+  version "1.3.0"
+  sha256 "958c6fa435a72e44bcce47ff8824fbfa81cd10c29a8b6f090b99de435303e45f"
 
+  # download.todesktop.com was verified as official when first introduced to the cask
   url "https://download.todesktop.com/200114584r0xzxa/anaba-#{version}.dmg"
-  name 'anaba'
-  homepage 'https://anaba.fr/'
+  name "anaba"
+  homepage "https://anaba.fr/"
 
-  app 'anaba.app'
+  app "anaba.app"
 end
